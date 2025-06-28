33db682984f3b3b956c557251485040c39f35b0c
takascemberi okeycini <takascemberi@gmail.com>
2025-06-28 22:02:47 +0000

    page.tsx düzeltildi, build hatası giderildi

--- src/app/[roomId]/page.tsx
+++ src/app/[roomId]/page.tsx
@@ -1,13 +1,15 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
 interface RoomPageProps {
-  params: { roomId: string };
+  params: {
+    roomId: string;
+  };
 }
 
 export default function RoomPage({ params }: RoomPageProps) {
+  const { roomId } = params;
+
   return (
     <div style={{ padding: 20, color: 'white' }}>
-      <h1>Oda ID: {params.roomId}</h1>
+      <h1>Oda ID: {roomId}</h1>
       <p>Dinamik rota çalışıyor.</p>
     </div>
   );
