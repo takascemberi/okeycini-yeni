63a168d25be59fa9181c7658f9ffd30197bb9301
takascemberi okeycini <takascemberi@gmail.com>
2025-06-29 00:16:55 +0000

    Add homepage to fix 404 error

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Okey Cini</h1>
+      <p>Hoş geldin! <a href='/auth/login'>Giriş yapmak için tıkla</a></p>
+    </div>
+  );
+}
