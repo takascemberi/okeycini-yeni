3631011bdfafef2380e6eed61ad82a4c58bc71cf
takascemberi okeycini <takascemberi@gmail.com>
2025-06-29 01:33:18 +0000

    Fix Firebase storageBucket typo

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAFX7BWtHyr5nmV0rj36RgQz41qJ2NyRLo",
   authDomain: "okeycini-ea22e.firebaseapp.com",
   projectId: "okeycini-ea22e",
-  storageBucket: "okeycini-ea22e.firebasestorage.app",
+  storageBucket: "okeycini-ea22e.appspot.com", // ✅ DÜZELTİLDİ
   messagingSenderId: "102049886702",
   appId: "1:102049886702:web:befef059dfd2086d375eff",
 };
